b33e999a2e4ffe083cc76d80b6e4ad8519cb688c
Jumi Yoon <jums.code@gmail.com>
2023-01-12 17:35:12 -0800

    commit tree 453b61b9748cd6837f039f19c02204fe6892245e

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import MoviesList from "./MoviesList";
+import { Route, useRouteMatch  } from "react-router-dom";
+import MoviesList from "./MoviesList.js";
+import MovieShow from "./MovieShow.js";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+
+      <Route path={`${match.url}/:movieID`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
 }
